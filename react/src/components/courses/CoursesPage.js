db3ae5d67a5f1de73ba3f730cdfb8e6366700a69
AAKASH HANDA <AAKASH HANDA>
2017-08-05 10:11:55 +0530

    A

--- react/src/components/courses/CoursesPage.js
+++ react/src/components/courses/CoursesPage.js
@@ -3,11 +3,31 @@ import {Link} from 'react-router';
 
 
 class CoursesPage extends React.Component{
+	constructor(props, context ){
+		super(props, context);
+
+		this.state ={
+			course:{title:""}
+		};
+		this.onTitleChange = this.onTitleChange.bind(this);
+		this.onClickSave = this.onClickSave.bind(this);
+	}
+	onTitleChange(event){
+		const course = this.state.course;
+		course.title = event.target.value;
+		this.setState({course:course});
+	}
+	onClickSave(){
+		alert(`text is ${this.state.course.title}`)
+	}
 	render(){
 		return(
 			<div className= "jumbotron">
-				<h1>This is course</h1>
-				<Link to="about" className="btn btn-primary">More About</Link>
+				<h1>Courses</h1>
+				<h2>Add Course</h2>
+				<input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
+				<input type="submit" onClick={this.onClickSave} />
+				
 				
 			</div>
 		);
